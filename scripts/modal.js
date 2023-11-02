050c99fff114699efc311dd1fe422744fd04caa8
stelda <stelda@outlook.fr>
2023-11-02 21:11:44 +0100

    Enable modal window closure

---
+++ scripts/modal.js
@@ -0,0 +1,31 @@
+/**
+ * opening and closing the modal
+ */
+
+/** Variables **/
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
+
+
+/** Functions **/
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
+/** Events **/
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+modalClose.addEventListener("click", closeModal);
+
