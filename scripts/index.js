7fad46557e0a4e9e41859ca36bf9d306a0b5d42c
stelda <stelda@outlook.fr>
2023-11-02 21:09:27 +0100

    Refactor code

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+/**
+ * add a class to the topnav element to make it responsive
+ */
+
+function editNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
